defaa72c7df92ac615d3d80ebc397862bf7a24e1
LezdCS <xxeurobluexx@gmail.com>
2024-04-16 18:20:07 +0900

    Change from chatroomId to username to connect to a chat

--- lib/src/kick_chat.dart
+++ lib/src/kick_chat.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
-import 'package:kick_chat/src/entities/kick_user.dart';
+import 'package:kick_chat/kick_chat.dart';
 import 'package:web_socket_channel/io.dart';
 
 class KickChat {
-  String chatroomId;
+  String username;
 
   IOWebSocketChannel? _webSocketChannel;
   StreamSubscription? _streamSubscription;
@@ -18,17 +18,21 @@ class KickChat {
   Stream get chatStream => _chatStreamController.stream;
 
   KickChat(
-    this.chatroomId, {
+    this.username, {
     this.onDone,
     this.onError,
   });
 
   Future<void> connect() async {
+    userDetails = await KickApi.getUserDetails(username);
+    if(userDetails == null) {
+      return;
+    }
 
     _webSocketChannel = IOWebSocketChannel.connect(
         "wss://ws-us2.pusher.com/app/eb1d5f283081a78b932c?protocol=7&client=js&version=7.6.0&flash=false");
     _webSocketChannel?.sink.add(
-        '{"event":"pusher:subscribe","data":{"auth":"","channel":"chatrooms.$chatroomId.v2"}}');
+        '{"event":"pusher:subscribe","data":{"auth":"","channel":"chatrooms.${userDetails!.id}.v2"}}');
 
     _streamSubscription = _webSocketChannel?.stream.listen(
       (data) => _chatListener(data),
--- test/kick_chat_test.dart
+++ test/kick_chat_test.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:kick_chat/kick_chat.dart';
-import 'package:kick_chat/src/entities/kick_user.dart';
-import 'package:kick_chat/src/kick_chat.dart';
-import 'package:kick_chat/src/events/kick_message.dart';
-import 'package:kick_chat/src/event_parser.dart';
-import 'package:kick_chat/src/kick_event.dart';
 
 
 void main() {
-  String chatRoomId = '3165515';
+  String username = 'xqc';
   test('Listen to a Kick chat', () async {
-    KickChat chat = KickChat(chatRoomId);
+    KickChat chat = KickChat(username);
     chat.connect();
     chat.chatStream.listen((message) {
       final KickEvent? kickEvent = eventParser(message);
